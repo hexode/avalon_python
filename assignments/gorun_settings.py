2a0c11c4551495b64d87d6813adedadcccf1186b
hexode <usr.noid@gmail.com>
2013-11-05 16:13:47 +0000

    exersize 2 completed

--- assignments/gorun_settings.py
+++
@@ -1,6 +0,0 @@
-# gorun settings
-DIRECTORIES = (
-    ('.', './test_runner.py'),
-)
-
-IGNORE_EXTENSIONS = ('log', 'pyc', 'pyo', 'swp', 'swo')
---
+++ gorun_settings.py
@@ -0,0 +1,9 @@
+# gorun settings
+test_runner = './test_runner.py'
+
+DIRECTORIES = (
+    ('avalon_python', test_runner),
+    ('test', test_runner),
+)
+
+IGNORE_EXTENSIONS = ('log', 'pyc', 'pyo', 'swp', 'swo')
