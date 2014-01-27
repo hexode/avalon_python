2a5cec461ad78427caa01e2aba930513c4f34fa7
hexode <usr.noid@gmail.com>
2014-01-27 14:40:37 +0400

    pull request fixes

--- avalon_python/control_structures/num_rel.py
+++ avalon_python/control_structures/num_rel.py
@@ -26,33 +26,38 @@
 
 from itertools import combinations
 from math import log, sqrt
-
+from operator import add, sub, mul, floordiv, pow as power
 import argparse
 import sys
 import re
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Find relation between numbers')
-    parser.add_argument('-f', '--filename', dest="filename", help='read combinations from FILE')
-    parser.add_argument("-s", "--separator", dest="separator", help="separator between numbers")
+    parser = argparse.ArgumentParser(
+        description='Find relation between numbers'
+    )
+    parser.add_argument('-f', '--filename', dest="filename",
+                        help='read combinations from FILE')
+    parser.add_argument("-s", "--separator", dest="separator",
+                        help="separator between numbers")
+    parser.add_argument("-u", "--unique", dest="unique", action="store_true",
+                        help="exclude repeated numbers")
 
-    parser.add_argument("-u", "--unique", dest="unique", action="store_true", help="exclude repeated numbers")
+    options = parser.parse_args()
 
-    options = parser.parse_args().__dict__
-
-    separator = options['separator'] or r';|,'
+    separator = options.separator or r';|,'
 
     sequence = []
 
-    filename = options['filename']
+    filename = options.filename
     if filename:
         try:
             with open(filename, 'r') as f:
                 for line in f:
                     sequence.extend(re.split(separator, line))
         except IOError as e:
-            print "Error:%s:reading %s:%s" % (sys.argv[0], e.filename, e.strerror)
+            print "Error:%s:reading %s:%s" % (sys.argv[0], e.filename,
+                                              e.strerror)
             sys.exit(1)
 
     if not len(sequence):
@@ -60,16 +65,16 @@ def main():
         sequence = re.split(separator, user_input)
 
     sequence = map(int, sequence)
-    if options['unique']:
+    if options.unique:
         sequence = set(sequence)
 
     binar_combinations = combinations(sequence, 2)
 
-    sub = lambda x: x[0] - x[1]
-    add = lambda x: x[0] + x[1]
-    mul = lambda x: x[0] * x[1]
-    div = lambda x: float(x[0]) / x[1]
-    power = lambda x: x[0] ** x[1]
+   # sub = lambda x: x[0] - x[1]
+   # add = lambda x: x[0] + x[1]
+   # mul = lambda x: x[0] * x[1]
+   # div = lambda x: float(x[0]) / x[1]
+   # power = lambda x: x[0] ** x[1]
 
     log2 = lambda x: log(x, 2)
 
@@ -79,7 +84,7 @@ def main():
         (mul, 'mul', True),
 
         (sub, 'sub', False),
-        (div, 'div', False),
+        (floordiv, 'div', False),
         (power,  'power', False)
     ]
 
@@ -90,23 +95,28 @@ def main():
 
     def get_relation(args, fn, sequence):
         try:
-            result = fn(args)
+            if type(args) == tuple:
+                result = fn(*args)
+            else:
+                result = fn(args)
             return (result, result in sequence)
         except:
-            pass
-        return (None, False)
+            return (None, False)
 
     binar_relations = []
     for arg_pair in binar_combinations:
         for func, func_name, is_commutative in binar_func_list:
-            relation_number, has_relation = get_relation(arg_pair, func, sequence)
+            relation_number, has_relation = get_relation(arg_pair,
+                                                         func, sequence)
             if has_relation:
                 binar_relations.append(arg_pair + (func_name, relation_number))
 
             if not is_commutative:
-                relation_number, has_relation = get_relation(arg_pair[::-1], func, sequence)
+                relation_number, has_relation = get_relation(arg_pair[::-1],
+                                                             func, sequence)
                 if has_relation:
-                    binar_relations.append(arg_pair[::-1] + (func_name, relation_number))
+                    binar_relations.append(arg_pair[::-1] + (func_name,
+                                                             relation_number))
 
     unar_relations = []
     for arg in sequence:
@@ -115,11 +125,10 @@ def main():
             if has_relation:
                 unar_relations.append((arg, func_name, relation_number))
 
-
-    print 'Binar relations:'
+    print 'Binary relations:'
     print '=' * 20
     print '\n'.join(map(lambda rel: '%-5d%-5d%-7s%-5d' % rel, binar_relations))
-    print '\nUnar relations:'
+    print '\nUnary relations:'
     print '=' * 20
     print '\n'.join(map(lambda rel: '%-5d%-7s%-5d' % rel, unar_relations))
 
